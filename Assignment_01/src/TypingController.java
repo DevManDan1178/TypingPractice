e8d901a7df279c2f30fe713e1ba867e16e880bcc
DevManDan1178 <l3r0d4niel@gmail.com>
2025-10-05 13:23:12 -0400

    + Visual keyboard display

--- Assignment_01/src/TypingController.java
+++ Assignment_01/src/TypingController.java
@@ -1,12 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+import java.util.Map;
+import java.util.function.Function;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 /**
  *
  * @author nagatd
  */
 public class TypingController {
-    // TODO All UI logic and event handling should go here
+
+    private static final String[][] keyboardCharacters = {
+        {"Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\"}, 
+        {"A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "\'"},
+        {"SHIFT", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"}
+    };
+    
+    public void DisplayOntoRootPane(GridPane rootPane) {
+        for (int i = 0; i < keyboardCharacters.length; i++) {
+            String[] rowCharacters = keyboardCharacters[i];
+            for (int j = 0; j < rowCharacters.length; j++) {
+                String keyboardCharacter = rowCharacters[j];
+                Button keyButton = new Button(keyboardCharacter);
+                keyButton.setMinSize(50, 50);
+                rootPane.add(keyButton, j, i);
+                
+            }
+        }
+    }
 }
