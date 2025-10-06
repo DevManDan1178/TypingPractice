b8b252ae8da5bbbcdd5d78a95f22c856081ababa
DevManDan1178 <l3r0d4niel@gmail.com>
2025-10-05 23:28:19 -0400

    COMPLETE VERSION 1 + Added TAB and CAPS LOCK implementation, with error popup on invalid input

--- Assignment_01/src/MainApp.java
+++ Assignment_01/src/MainApp.java
@@ -3,18 +3,9 @@
  *
  * @author Daniel Orejuela Liu
  */
-import java.util.HashMap;
 import javafx.application.Application;
-import javafx.event.EventType;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
