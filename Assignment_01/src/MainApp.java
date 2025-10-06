fa9764e16af2afb08add26b44aecbfc5839284cd
DevManDan1178 <l3r0d4niel@gmail.com>
2025-10-05 23:02:21 -0400

    COMPLETE VERSION 1 + Error counting + Controls

--- Assignment_01/src/MainApp.java
+++ Assignment_01/src/MainApp.java
@@ -1,3 +1,4 @@
+
 /**
  *
  * @author Daniel Orejuela Liu
@@ -8,59 +9,46 @@ import javafx.event.EventType;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+public class MainApp extends Application {
+    //MainApp.java launches the app and sets up the main scene.
 
-
-public class MainApp extends Application{
-         //MainApp.java launches the app and sets up the main scene.
-
+    
+    private static String[] PRACTICE_STRINGS = {
+        "Try typing this text. Do it as quickly and accurately as you can.",
+        "Next type another line of input data.",
+        "The quick brown fox jumps over the lazy dog.",
+        "Five big quacking zephyrs jolt my wax bed.",
+        "Sympathizing would fix Quaker objectives.",
+        "A large fawn jumped quickly over white zinc boxes."
+    };
     /**
      * @param args the command line arguments
      */
-       public static void main(String[] args) {
+    public static void main(String[] args) {
         launch(args);
     }
-     
-    
+
     @Override
     public void start(Stage primaryStage) {
-        
+
         primaryStage.setTitle("Typing Tutor - JavaFX Assignment");
 
-        Pane root = new Pane();
-        
-        GridPane keyboardGrid = new GridPane();
-        root.getChildren().add(keyboardGrid);
-        HashMap<String, Button> keyboardMap =  TypingController.DisplayOntoRootPane(keyboardGrid);
-        
-        Scene scene = new Scene(root, 800, 600);
-        scene.setOnKeyPressed((KeyEvent event) -> {
-            KeyCode pressedKey = event.getCode();
-            String keyStr = pressedKey.toString();
-            Button keyboardButton = keyboardMap.get(keyStr);
-            if (keyboardButton == null) {
-                return;
-            }
-            keyboardButton.setStyle("-fx-background-color: #ff0000;");
-        });
-        scene.setOnKeyReleased((KeyEvent event) -> {
-            KeyCode pressedKey = event.getCode();
-            String keyStr = pressedKey.toString();
-            Button keyboardButton = keyboardMap.get(keyStr);
-            if (keyboardButton == null) {
-                return;
-            }
-            keyboardButton.setStyle("");
-        });
+        BorderPane root = new BorderPane();
+        TypingController typingController = new TypingController(PRACTICE_STRINGS);
+        root.setCenter(typingController.getRoot());
+        Scene scene = new Scene(root, 900, 600);
+        scene.setOnKeyPressed(typingController.getKeyEventHandler(true));
+        scene.setOnKeyReleased(typingController.getKeyEventHandler(false));
         primaryStage.setScene(scene);
         primaryStage.show();
-        
-        
-       
+
     }
 }
